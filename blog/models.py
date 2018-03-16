fbf95127397d89bca7b931e436bbfcfeea35e998
Jonathan Behrens <jbehrens709@gmail.com>
2018-03-16 09:03:19 -0400

    My Blog App, first commit

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+#create class post to give information on blog posters
+#include methods to publish posts in class
+
+class post(models.Model):
+  author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+  title = models.CharField(max_length=200)
+  text = models.TextField()
+  created_date = models.DateTimeField(default=timezone.now)
+  published_date = models.DateTimeField(blank=True, null=True)
+
+  def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+  
+  def __str__(self):
+    return self.title
+
+
